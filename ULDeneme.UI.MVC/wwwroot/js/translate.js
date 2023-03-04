5cf14c22db388de0124ed36bb4e0e4acd8c80121
GBaris <126857159+GBaris@users.noreply.github.com>
2023-03-04 18:12:13 +0300

    API için JS kodalrı oluşuturuldu, create.cshml için kullanılacaktı ama muhtemelen silinecek.

--- ULDeneme.UI.MVC/wwwroot/js/translate.js
+++ ULDeneme.UI.MVC/wwwroot/js/translate.js
@@ -1,41 +1,50 @@
-﻿namespace ULDeneme.UI.MVC.wwwroot.js
-{
-    public class translate
-    {
-        $(document).ready(function() {
-            var input = $("#UnKVoc");
-            var suggestions = $("#suggestions");
+﻿function init() {
+    var input = $("#UnKVoc");
+    var suggestions = $("#suggestions");
 
-            input.keyup(function () {
-                var text = input.val();
-                var fromLang = "@ViewBag.KnownLangShort";
-                var toLang = "@ViewBag.UnknownLangShort";
+    input.on("input", function () {
+        var text = input.val();
+        var fromLang = "@ViewBag.KnownLangShort";
+        var toLang = "@ViewBag.UnknownLangShort";
 
-                $.ajax({
-                    type: "POST",
-                    url: "/Translation/Translate",
-                    data: { textToTranslate: text, fromLang: fromLang, toLang: toLang },
-                    success: function (data) {
-                        var words = data.split(" ");
-                        var lastWord = words[words.length - 1];
-                        if (lastWord.length > 2) {
-                            $.ajax({
-                                type: "POST",
-                                url: "/Translation/Suggest",
-                                data: { word: lastWord, fromLang: fromLang, toLang: toLang },
-                                success: function (data) {
-                                    suggestions.empty();
-                                    for (var i = 0; i < data.length; i++) {
-                                        suggestions.append("<div>" + data[i] + "</div>");
-                                    }
-                                }
-                            });
-                        } else {
-                            suggestions.empty();
+        $.ajax({
+            type: "POST",
+            url: "/Translation/Translate",
+            data: { textToTranslate: text, fromLang: fromLang, toLang: toLang },
+            success: function (data) {
+                var words = data.split(" ");
+                var lastWord = words[words.length - 1];
+                if (lastWord.length > 2) {
+                    $.ajax({
+                        type: "POST",
+                        url: "/Translation/Suggest",
+                        data: { word: lastWord, fromLang: fromLang, toLang: toLang },
+                        success: function (data) {
+                            var suggestionList = $("#suggestion-list");
+                            suggestionList.empty();
+                            if (data.length > 0) {
+                                suggestionList.append("<div class='suggestion'>" + data[0] + "</div>");
+                                suggestions.show();
+                            }
+                            else {
+                                suggestions.hide();
+                            }
                         }
-                    }
-                });
-            });
+                    });
+                } else {
+                    suggestions.hide();
+                }
+            }
         });
-    }
+    });
+
+    suggestions.on("click", ".suggestion", function () {
+        var suggestion = $(this).text();
+        $("#KVoc").val(suggestion);
+        suggestions.hide();
+    });
+
+    input.blur(function () {
+        suggestions.hide();
+    });
 }
